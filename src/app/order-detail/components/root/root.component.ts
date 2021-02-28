099171bf3455aae10ec340c145abb5636ddb4902
Wilbert Navarrete <wilmion92@gmail.com>
2021-02-28 01:13:29 +0100

    terminado de la pagina de detalles de ordenes

--- src/app/order-detail/components/root/root.component.ts
+++ src/app/order-detail/components/root/root.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute , Params } from '@angular/router';
+
+import { IDateTime, IOrder , IScheduleData, IUser } from '../../../core/models/interfaces';
+import { ApiService } from '../../../core/services/api.service';
 
 @Component({
   selector: 'app-root',
@@ -7,9 +11,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RootComponent implements OnInit {
 
-  constructor() { }
+  //params
+  user_name:string = 'NONE';
+  order_id:number = -1;
+
+  //order
+  order:IOrder | undefined;
+  user: IUser| undefined;
+
+
+  constructor(
+    private router:ActivatedRoute,
+    private ApiService : ApiService
+  ) { }
 
   ngOnInit(): void {
+    this.router.params.subscribe((params:Params) => {
+      this.user_name = params.user;
+      this.order_id = Number(params.id);
+      this.getOrder();
+    })
+  }
+  getOrder():void {
+    this.ApiService.getUsers()
+    .subscribe(users => {
+      const user:IUser | undefined = users.find(u => u.name.toLowerCase() === this.user_name);
+      if(user){
+        const schedules:IScheduleData = <IScheduleData> user.schedules.find(s => s.id === this.order_id);
+
+        this.order = {
+          ...schedules,
+          user_name: user.name
+        }
+        this.user = user;
+      }else {
+        alert('error');
+      }
+      
+    })
   }
 
+  //computed
+
+  buildDate(dateTime:IDateTime):string {
+    const date = dateTime.date;
+    const month = dateTime.month;
+    const year = dateTime.year; 
+    return `${date} / ${month} / ${year}`;
+  }
 }
--- src/app/order-detail/components/user-detail/user-detail.component.ts
+++ src/app/order-detail/components/user-detail/user-detail.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit , Input } from '@angular/core';
+import { IUser } from 'src/app/core/models/interfaces';
 
 @Component({
   selector: 'app-user-detail',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserDetailComponent implements OnInit {
 
+  @Input() user:IUser | undefined;
+
   constructor() { }
 
   ngOnInit(): void {
+    
   }
 
 }
