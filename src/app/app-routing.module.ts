6f5aa01df6ee51de895c0a7999bbfde4707c6e59
Wilbert Navarrete <wilmion92@gmail.com>
2021-07-03 04:34:00 +0200

    adding 404 error

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,73 +9,100 @@ import { LoginGuard } from './core/guards/login/login.guard';
 
 const routes: Routes = [
   {
-    path: "",
+    path: '',
     component: LayoutComponent,
     children: [
       {
-        path: "",
-        loadChildren : () => import('./home/home.module').then(m => m.HomeModule)
+        path: '',
+        loadChildren: () =>
+          import('./home/home.module').then((m) => m.HomeModule),
       },
       {
-        path: "product/:id",
-        loadChildren: () => import('./product-detail/product-detail.module').then(m => m.ProductDetailModule)
+        path: 'product/:id',
+        loadChildren: () =>
+          import('./product-detail/product-detail.module').then(
+            (m) => m.ProductDetailModule
+          ),
       },
       {
-        path: "checkout/order",
-        loadChildren: () => import('./order/order.module').then(m => m.OrderModule),
-        canActivate: [LoginGuard]
+        path: 'checkout/order',
+        loadChildren: () =>
+          import('./order/order.module').then((m) => m.OrderModule),
+        canActivate: [LoginGuard],
       },
       {
-        path: "admin/:action",
-        loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule),
-        canActivate: [AdminGuardGuard]
+        path: 'admin/:action',
+        loadChildren: () =>
+          import('./admin/admin.module').then((m) => m.AdminModule),
+        canActivate: [AdminGuardGuard],
       },
       {
-        path: "admin/items/edit/:id",
-        loadChildren: () => import('./post-product/post-product.module').then(m => m.PostProductModule),
-        canActivate: [AdminGuardGuard]
+        path: 'admin/items/edit/:id',
+        loadChildren: () =>
+          import('./post-product/post-product.module').then(
+            (m) => m.PostProductModule
+          ),
+        canActivate: [AdminGuardGuard],
       },
       {
-        path: "order/:user/:id",
-        loadChildren: () => import('./order-detail/order-detail.module').then(m => m.OrderDetailModule)
+        path: 'order/:user/:id',
+        loadChildren: () =>
+          import('./order-detail/order-detail.module').then(
+            (m) => m.OrderDetailModule
+          ),
       },
       {
-        path: "cart",
-        loadChildren: () => import('./cart/cart.module').then(m => m.CartModule),
-        canActivate: [LoginGuard]
+        path: 'cart',
+        loadChildren: () =>
+          import('./cart/cart.module').then((m) => m.CartModule),
+        canActivate: [LoginGuard],
       },
       {
-        path: "profile",
-        loadChildren: () => import('./profile/profile.module').then(m => m.ProfileModule),
-        canActivate: [LoginGuard]
+        path: 'profile',
+        loadChildren: () =>
+          import('./profile/profile.module').then((m) => m.ProfileModule),
+        canActivate: [LoginGuard],
       },
       {
-        path: "howitworks",
-        loadChildren: () => import('./how-it-works/how-it-works.module').then(m => m.HowItWorksModule)
+        path: 'howitworks',
+        loadChildren: () =>
+          import('./how-it-works/how-it-works.module').then(
+            (m) => m.HowItWorksModule
+          ),
       },
       {
-        path: "contact",
-        loadChildren: () => import('./contact-bussines/contact-bussines.module').then(m => m.ContactBussinesModule)
+        path: 'contact',
+        loadChildren: () =>
+          import('./contact-bussines/contact-bussines.module').then(
+            (m) => m.ContactBussinesModule
+          ),
       },
       {
-        path: "about",
-        loadChildren: () => import('./contact/contact.module').then(m => m.ContactModule)
-      }
-    ]
+        path: 'about',
+        loadChildren: () =>
+          import('./contact/contact.module').then((m) => m.ContactModule),
+      },
+      {
+        path: '**',
+        loadChildren: () =>
+          import('./error404/error404.module').then((m) => m.Error404Module),
+      },
+    ],
   },
   {
-    path: "auth/:mode",
-    loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule),
-    canActivate: [LoginGuard]
+    path: 'auth/:mode',
+    loadChildren: () => import('./auth/auth.module').then((m) => m.AuthModule),
+    canActivate: [LoginGuard],
   },
   {
-    path: "**",
-    loadChildren: () => import('./error404/error404.module').then(m => m.Error404Module)
-  }
+    path: '**',
+    loadChildren: () =>
+      import('./error404/error404.module').then((m) => m.Error404Module),
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
